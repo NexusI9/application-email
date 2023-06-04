d6e6ce05e291a92bdffe00822f345ab33bc812ac
NexusI9 <nassim.elkhantour@gmail.com>
2023-06-04 11:59:02 -0400

    add swatch

--- newsletter/emails/news-letter.tsx
+++ newsletter/emails/news-letter.tsx
@@ -19,7 +19,19 @@ import {
   ? `https://${process.env.VERCEL_URL}`
   : '';
 
+  const color = {
+    purple:'#5D47FF',
+    marine:'#182449',
+    void:'#070812',
+    dark_grey:'#444D66',
+    blue_white:'#F5F7F9',
+    white:'#ffffff'
+  }
   
+  const main= {
+    backgroundColor: color.blue_white,
+    fontFamily: 'HelveticaNeue,Helvetica,Arial,sans-serif',
+  };
 
   export const Newsletter = ({agencyName='agencyName'}:NewsletterProps) => (
     <Html>
